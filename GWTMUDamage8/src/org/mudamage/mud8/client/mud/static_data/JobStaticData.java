8291870351f5e9e148b9c8fb1fbf72e5d3762400
abitfire <abitfire@5450c723-22eb-8cc7-e3e6-6b817c0dad10>
2010-07-11 05:29:58 +0000

    データを別パッケージに変更。

---
+++ GWTMUDamage8/src/org/mudamage/mud8/client/mud/static_data/JobStaticData.java
@@ -0,0 +1,77 @@
+package org.mudamage.mud8.client.mud.static_data;
+/**
+ * Jobに関するデータ
+ * @author SINLION
+ *
+ */
+public class JobStaticData {
+	public static final Integer KNIGHT_NUM = 0;
+	public static final Integer WIZARD_NUM = 1;
+	public static final Integer ELF_NUM = 2;
+	public static final Integer MAGIC_NUM = 3;
+	public static final Integer DARKLOAD_NUM = 4;
+	public static final Integer SUMMONER_NUM = 5;
+	/**
+	 * ナイトを表す文字列
+	 */
+	public static final String KNIGHT = "ナイト";
+	/**
+	 * ウィザードを表す文字列
+	 */
+	public static final String WIZARD = "ウィザード";
+	/**
+	 * エルフを表す文字列
+	 */
+	public static final String ELF = "エルフ";
+	/**
+	 * 魔剣士を表す文字列
+	 */
+	public static final String MAGIC = "魔剣士";
+	/**
+	 * ダークロードを表す文字列
+	 */
+	public static final String DARKLOAD = "ダークロード";
+	/**
+	 * 召喚師を表す文字列
+	 */
+	public static final String SUMMONER = "召喚師";
+	/**
+	 * 全ての職業の文字列を格納した配列
+	 */
+	public static String[][] jobs = {
+		{KNIGHT,KNIGHT_NUM.toString()},
+		{WIZARD,WIZARD_NUM.toString()},
+		{ELF,ELF_NUM.toString()},
+		{MAGIC,MAGIC_NUM.toString()},
+		{DARKLOAD,DARKLOAD_NUM.toString()},
+		{SUMMONER,SUMMONER_NUM.toString()}
+	};
+	/**
+	 * 各職業に対応した数字を返します。
+	 * @param job
+	 * @return job_number
+	 */
+	public static int getJobNumber(String job){
+		for(int i=0;i<jobs.length;i++)
+			if(jobs[i].equals(job))
+				return i;
+		return -1;
+	}
+	/**
+	 * 各職業に対応した数字から各職業の文字列を返します。
+	 * @param jobnum
+	 * @return job_name
+	 */
+	/*
+	public static String getJobString(int jobnum){
+		return jobs[jobnum];
+	}
+	*/
+	/**
+	 * 全ての職業の文字列を格納した配列を返します。
+	 * @return jobs
+	 */
+	public static String[][] getJobs(){
+		return jobs;
+	}
+}
---
+++ GWTMUDamage8/src/org/mudamage/mud8/client/mud/static_data/WingStaticDataStructure.java
@@ -0,0 +1,67 @@
+package org.mudamage.mud8.client.mud.static_data;
+/**
+ * 羽のデータベース構造
+ * @author SINLION
+ *
+ */
+public class WingStaticDataStructure {
+	/**
+	 * 羽のデータベースを作成
+	 * @param name
+	 * @param jobs
+	 * @param ops
+	 * @param type
+	 * @param def
+	 * @param inc
+	 * @param dec
+	 */
+	public WingStaticDataStructure(String name,String jobs,String ops,Integer type,Integer def,Integer inc,Integer dec){
+		this.name = name;
+		this.jobs = jobs.split(",");
+		this.ops = ops.split(",");
+		this.type = type;
+		this.def = def;
+		this.inc = inc;
+		this.dec = dec;
+	}
+	/**
+	 * 名前
+	 */
+	public String name;
+	/**
+	 * 装備可能職
+	 */
+	public String[] jobs;
+	/**
+	 * オプションの種類(攻撃,魔力,防御,呪い)
+	 */
+	public String[] ops;
+	/**
+	 * 羽のタイプ<br>
+	 * 0:なし<br>
+	 * 1:1次羽<br>
+	 * 2:2次羽<br>
+	 * 3:3次羽,帝王のマント<br>
+	 * 4:君主のマント<br>
+	 */
+	public Integer type;
+	/**
+	 * Nでの防御力
+	 */
+	public Integer def;
+	/**
+	 * Nでのダメージ増加％
+	 */
+	public Integer inc;
+	/**
+	 * Nでのダメージ吸収％
+	 */
+	public Integer dec;
+	
+	public Boolean equalJobs(String job){
+		for(String j : jobs)
+			if(j.equals(job))
+				return true;
+		return false;
+	}
+}
