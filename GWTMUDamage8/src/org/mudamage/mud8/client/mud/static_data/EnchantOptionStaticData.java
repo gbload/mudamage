bd68cef04b0e6e066721b44da0f5702c378ba490
abitfire <abitfire@5450c723-22eb-8cc7-e3e6-6b817c0dad10>
2010-07-14 01:14:24 +0000

    エンチャントOPのデータを作成

--- GWTMUDamage8/src/org/mudamage/mud8/client/mud/static_data/EnchantOptionStaticData.java
+++ GWTMUDamage8/src/org/mudamage/mud8/client/mud/static_data/EnchantOptionStaticData.java
@@ -42,14 +42,14 @@ public class EnchantOptionStaticData {
 	public static final String[] CRI_NAME = {"Cダメ",CRI.toString()};
 	public static final String[] SKILL_NAME = {"スキル攻撃力",SKILL.toString()};
 	public static final String[] PVP_HIT_NAME = {"対人攻撃率",PVP_HIT.toString()};
-	public static final String[] SD_DEC_NAME = {"SD比率減",SD_DEC.toString()};
-	public static final String[] SD_IGNORE_NAME = {"SD無視",SD_IGNORE.toString()};
+	public static final String[] SD_DEC_NAME = {"SD比率減(%)",SD_DEC.toString()};
+	public static final String[] SD_IGNORE_NAME = {"SD無視(%)",SD_IGNORE.toString()};
 	// 杖エンチャント
 	public static final String[] MAGIC_NAME = {"魔力上昇",MAGIC.toString()};
 	public static final String[] MAGIC_SKILL_NAME = {"スキル攻撃力",MAGIC_SKILL.toString()};
 	public static final String[] MAGIC_CRI_NAME = {"Cダメ",MAGIC_CRI.toString()};
-	public static final String[] MAGIC_SD_DEC_NAME = {"SD比率減",MAGIC_SD_DEC.toString()};
-	public static final String[] MAGIC_SD_IGNORE_NAME = {"対人比率減",MAGIC_SD_IGNORE.toString()};
+	public static final String[] MAGIC_SD_DEC_NAME = {"SD比率減(%)",MAGIC_SD_DEC.toString()};
+	public static final String[] MAGIC_SD_IGNORE_NAME = {"SD無視(%)",MAGIC_SD_IGNORE.toString()};
 	// 防具エンチャント
 	public static final String[] DEF_NAME = {"防御力上昇",DEF.toString()};
 	public static final String[] AG_NAME = {"最大AG上昇",AG.toString()};
@@ -57,9 +57,71 @@ public class EnchantOptionStaticData {
 	public static final String[] AH_NAME = {"自動生命回復",AH.toString()};
 	public static final String[] AM_NAME = {"自動マナ回復",AM.toString()};
 	public static final String[] PVP_AVOID_NAME = {"対人防御成功",PVP_AVOID.toString()};
-	public static final String[] DEC_NAME = {"ダメ減",DEC.toString()};
-	public static final String[] SD_INC_NAME = {"SD比率上昇",SD_INC.toString()};
-	
+	public static final String[] DEC_NAME = {"ダメ減(%)",DEC.toString()};
+	public static final String[] SD_INC_NAME = {"SD比率上昇(%)",SD_INC.toString()};
+	/*
+	 * 定数　数値
+	 */
+	public static final Integer[][] values = {
+		{2,3,4,5,6,7,9,11,12,14,15,16,17,20}, // 最小攻撃力
+		{3,4,5,6,7,8,10,12,14,17,20,23,26,29}, // 最大攻撃力
+		{6,8,10,12,14,16,20,23,26,29,32,35,37,40}, // 要求力減
+		{6,8,10,12,14,16,20,23,26,29,32,35,37,40}, // 要求敏減
+		{7,8,9,11,12,14,16,19}, // 攻撃力
+		{12,14,16,18,20,22,24,30}, // Cダメ
+		{12,14,16,18,22}, // スキル攻撃力
+		{5,7,9,11,14}, // 対人攻撃率
+		{3,5,7,9,10}, // SD比率減%
+		{10}, // SD無視%
+		
+		{6,8,10,12,14,16,17,18,19,21,23,25,27,31}, // 魔力上昇
+		{7,10,13,16,19,22,25,30}, // 魔法スキル攻撃力
+		{10,12,14,16,18,20,22,28}, // 魔法クリティカルダメージ
+		{4,6,8,10,13}, // 魔法SD比率減%
+		{15}, // 魔法SD無視%
+		
+		{3,4,5,6,7,8,10,12,14,16,18,20,22,25}, // 防御力
+		{4,6,8,10,12,14,16,18,20,22,25}, // 最大AG
+		{7,9,11,13,15,17,19,21,23,25,30}, // 最大HP
+		{1,2,3,4,5,6,7,8}, // 自動生命回復
+		{1,2,3,4,5}, // 自動マナ回復
+		{3,4,5,6,8}, // 対人防御成功率
+		{3,4,5,6,7}, // ダメ減%
+		{5}, // SD比率上昇%
+	};
+	/*
+	 * 定数　要求LV
+	 */
+	public static final Integer[] require_lv = {
+		0, // 最大攻撃力
+		0, // 最大攻撃力
+		0, // 要求力減
+		0, // 要求敏減
+		6, // 攻撃力
+		6, // Cダメ
+		9, // スキル攻撃力
+		9, // 対人攻撃率
+		9, // SD比率減
+		13, // SD無視
+		
+		0, // 魔力上昇
+		6, // 魔法スキル攻撃力
+		6, // 魔法クリティカルダメージ
+		9, // 魔法SD比率減
+		13, // 魔法SD無視
+		
+		0, // 防御力
+		3, // 最大AG
+		3, // 最大HP
+		6, // 自動生命回復
+		9, // 自動マナ回復
+		9, // 対人防御成功率
+		9, // ダメ減
+		13 // SD比率上昇
+	};
+	/*
+	 * 定数　名前配列
+	 */
 	public static final String[][] enchant_attack_names = {
 		MIN_NAME,
 		MAX_NAME,
@@ -73,9 +135,47 @@ public class EnchantOptionStaticData {
 		SD_IGNORE_NAME
 	};
 	public static final String[][] enchant_magic_names = {
-		
+		MAGIC_NAME,
+		REQUIRE_STR_NAME,
+		REQUIRE_AGI_NAME,
+		MAGIC_CRI_NAME,
+		MAGIC_SKILL_NAME,
+		PVP_HIT_NAME,
+		MAGIC_SD_DEC_NAME,
+		MAGIC_SD_IGNORE_NAME
 	};
 	public static final String[][] enchant_guard_names = {
-		
+		DEF_NAME,
+		AG_NAME,
+		HP_NAME,
+		AH_NAME,
+		AM_NAME,
+		PVP_AVOID_NAME,
+		DEC_NAME,
+		SD_INC_NAME
 	};
+	/*
+	 * 名前の取得関数
+	 */
+	public static String[][] getEnchantAttackNames(){
+		return enchant_attack_names;
+	}
+	public static String[][] getEnchantMagicNames(){
+		return enchant_magic_names;
+	}
+	public static String[][] getEnchantGuardNames(){
+		return enchant_guard_names;
+	}
+	/*
+	 * 値の取得関数
+	 */
+	public static String[] getEnchanValues(int name,int plus){
+		int size = require_lv[name] - plus + 1;
+		if(size <= 0)return null;
+		String[] str = new String[size];
+		for(int i=0;i<size;i++){
+			str[i] = values[i].toString();
+		}
+		return str;
+	}
 }
