f44195ff293399fb49cea3ae719e1f5eec772f6a
abitfire <abitfire@5450c723-22eb-8cc7-e3e6-6b817c0dad10>
2010-07-15 01:09:48 +0000

    ソケットOPのデータを作成

--- trunk/GWTMUDamage8/src/org/mudamage/mud8/client/mud/static_data/SocketOptionStaticData.java
+++ trunk/GWTMUDamage8/src/org/mudamage/mud8/client/mud/static_data/SocketOptionStaticData.java
@@ -45,6 +45,15 @@ public class SocketOptionStaticData {
 	// 地
 	public static final Integer VIT = 50;
 	/*
+	 * 定数　属性
+	 */
+	public static final Integer FIRE = 0;
+	public static final Integer LIGHTNING = 1;
+	public static final Integer ICE = 2;
+	public static final Integer WATER = 3;
+	public static final Integer WIND = 4;
+	public static final Integer EARTH = 5;
+	/*
 	 * 定数　名前
 	 */
 	// 炎
@@ -81,31 +90,114 @@ public class SocketOptionStaticData {
 	// 地
 	public static final String[] VIT_NAME = {"体力増加",VIT.toString()};
 	/*
+	 * 定数　名前配列
+	 */
+	public static final String[][] attack_names = {
+		ATTACK_LV_NAME,
+		MAX_NAME,
+		MIN_NAME,
+		ATTACK_NAME,
+		SPEED_NAME,
+		AG_DEC_NAME,
+		EXD_DAMAGE_NAME,
+		EXD_PER_NAME,
+		CRI_DAMAGE_NAME,
+		CRI_PER_NAME,
+		SKILL_NAME,
+		GET_HP_NAME,
+		GET_MANA_NAME,
+		HIT_NAME,
+		ITEM_ENDURANCE_NAME
+	};
+	public static final String[][] guard_names = {
+		DEF_NAME,
+		SHIELD_NAME,
+		DEC_NAME,
+		REFLECTION_NAME,
+		AVOID_NAME,
+		HP_NAME,
+		MANA_NAME,
+		AG_MAX_NAME,
+		AH_NAME,
+		AM_NAME,
+		AG_AUTO_NAME,
+		VIT_NAME,
+	};
+	/*
 	 * 定数　値
 	 */
 	// 炎
 	public static final Integer[][] fire_values = {
-		
+		{20,19,18,17,16}, // 攻撃魔力LV
+		{30,33,36,39,42}, // 最大攻撃魔力
+		{20,22,24,26,28}, // 最小攻撃魔力
+		{20,22,24,26,28}, // 攻撃魔力増加
+		{7,8,9,10,11,12}, // 速度増加
+		{40,41,42,43,44}, // AG消費減少
 	};
 	// 稲妻
 	public static final Integer[][] lightning_values = {
-		
+		{15,16,18,19,21}, // EXD増加
+		{10,11,12,13,14}, // EXD確率
+		{30,33,36,39,42}, // クリ増加
+		{8,9,10,11,12}, // クリ確率
 	};
 	// 氷
 	public static final Integer[][] ice_values = {
-		
+		{37,40,43,46,49}, // スキル増加
+		{8,7,6,5,4}, // 獲得生命
+		{8,7,6,5,4}, // 獲得マナ
+		{25,27,30,32,35}, // 攻撃成功率
+		{30,31,32,33,34}, // アイテム耐久
 	};
 	// 水
 	public static final Integer[][] water_values = {
-		
+		{30,33,36,39,42}, // 防御力増加
+		{7,8,9,10,11}, // 盾装備時
+		{4,5,6,7,8}, // ダメ減
+		{5,6,7,8,9}, // ダメ反
+		{10,11,12,13,14}, // 防御成功
 	};
 	// 風
 	public static final Integer[][] wind_values = {
-		
+		{4,5,6,7,8}, // 最大生命増加
+		{4,5,6,7,8}, // 最大マナ増加
+		{25,27,30,32,35}, // 最大AG増加
+		{8,9,10,11,12}, // 自動生命回復
+		{7,8,9,10,11}, // 自動マナ回復
+		{3,4,5,6,7}, // 自動AG回復
 	};
 	// 地
 	public static final Integer[][] earth_values = {
-		
+		{30,33,36,39,42}, // 体力増加
 	};
-
+	public static final Integer[][][] values = {
+		fire_values,
+		lightning_values,
+		ice_values,
+		water_values,
+		wind_values,
+		earth_values
+	};
+	/*
+	 * 名前の取得関数
+	 */
+	public static String[][] getAttackNames(){
+		return attack_names;
+	}
+	public static String[][] getGuardNames(){
+		return guard_names;
+	}
+	/*
+	 * 値の取得関数
+	 */
+	public static String[] getSocketValues(int name){
+		String[] result = new String[5];
+		int attr = name/10;
+		int op = name%10;
+		Integer[] val = values[attr][op];
+		for(int i=0;i<5;i++)
+			result[i] = val[i].toString();
+		return result;
+	}
 }
