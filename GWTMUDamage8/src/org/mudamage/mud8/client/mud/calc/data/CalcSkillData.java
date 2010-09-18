6b3369ef061b67fd9a239be2ac74772659866867
abitfire <abitfire@5450c723-22eb-8cc7-e3e6-6b817c0dad10>
2010-09-18 10:59:02 +0000

    スキル計算追加。

---
+++ GWTMUDamage8/src/org/mudamage/mud8/client/mud/calc/data/CalcSkillData.java
@@ -0,0 +1,12 @@
+package org.mudamage.mud8.client.mud.calc.data;
+
+public class CalcSkillData {
+	public Integer name;
+	public Integer type;
+	public Integer skill_min; // スキル威力
+	public Integer skill_max; // スキル威力
+	public Integer min_damage; // スキルダメージ計算結果
+	public Integer max_damage; // スキルダメージ計算結果
+	public Integer cri_damage; // クリティカルダメージ計算結果
+	public Integer exd_damage; // エクセレントダメージ計算結果
+}
