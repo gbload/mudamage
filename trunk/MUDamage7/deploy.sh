f192d12687c1c661ad559118bf336acd11a5b7b7
abitfire <abitfire@5450c723-22eb-8cc7-e3e6-6b817c0dad10>
2010-09-22 15:01:06 +0000

    配置替え

--- trunk/MUDamage7/deploy.sh
+++ trunk/MUDamage7/deploy.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
-rm -rf ./mudamage
-cp -r ./bin-debug ./mudamage
-rm -rf ./mudamage/data
-rm -rf ./mudamage/img
-rm -rf ./mudamage*.zip
-zip -r ./mudamage.zip ./mudamage
-zip -r ./mudamage_ver$1.zip ./mudamage
-zip -r ./mudamage_src.zip ./src
-zip -r ./mudamage_src_ver$1.zip ./src
+rm -rf ./distribute/mudamage
+cp -r ./bin-debug ./distribute/mudamage
+
+rm -rf ./distribute/mudamage/data
+rm -rf ./distribute/mudamage/img
+rm -rf ./distribute/mudamage/.svn
+
+rm -rf ./distribute/mudamage*.zip
+zip -r ./distribute/mudamage.zip ./distribute/mudamage
+zip -r ./distribute/mudamage_ver$1.zip ./distribute/mudamage
+zip -r ./distribute/mudamage_src.zip ./src
+zip -r ./distribute/mudamage_src_ver$1.zip ./src
 # 外部データ
 cp ./src/external.as ./src/data.as
 ~/lib/flex_sdk_3.4.1.10084/bin/mxmlc ./src/MUDamage7.mxml -output ./bin-debug/MUDamage7.swf
-rm -rf ./mudamage_export
-cp -r ./bin-debug ./mudamage_export
-rm -rf ./mudamage_export/img
-zip -r ./mudamage_export.zip ./mudamage
-zip -r ./mudamage_export_ver$1.zip ./mudamage
+
+rm -rf ./distribute/mudamage_export
+cp -r ./bin-debug ./distribute/mudamage_export
+
+rm -rf ./distribute/mudamage_export/img
+rm -rf ./distribute/mudamage_export/.svn
+zip -r ./distribute/mudamage_export.zip ./distribute/mudamage_export
+zip -r ./distribute/mudamage_export_ver$1.zip ./distribute/mudamage_export
