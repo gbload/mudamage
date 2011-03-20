aa54349bbfb09b04234c7550a6711d811a883b8c
abitfire <abitfire@5450c723-22eb-8cc7-e3e6-6b817c0dad10>
2011-03-20 13:51:08 +0000

    英語に翻訳

--- MUDamage7/lang/en_embed.pl
+++ MUDamage7/lang/en_embed.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl -w
 $filename = $ARGV[0];
+$datafile = "result7.txt";
+
 open(FH,$filename);
 @dat = <FH>;
 close(FH);
 
-open(FH,"result.txt");
+open(FH,$datafile);
 @dat2 = <FH>;
 close(FH);
 
@@ -14,28 +16,49 @@ foreach(@dat2){
 
 my $len2 = @dat2;
 
-for($i=0;$i<$len2;$i+=2){
-	if($i+1 < $len2 && $dat2[$i+1] ne ""){
-		foreach(@dat){
-			$tmp1 = "\"$dat2[$i]\"";
-			$tmp1 =~ s/\(/\\\(/g;
-			$tmp1 =~ s/\)/\\\)/g;
-			$tmp1 =~ s/\[/\\\[/g;
-			$tmp1 =~ s/\]/\\\]/g;
-			$tmp2 = "\"$dat2[$i+1]\"";
-			# if($_ =~ /$tmp1/){print $_;}
-			$_ =~ s/$tmp1/$tmp2/g;
+#for($i=0;$i<$len2;$i+=2){
+#	if($i+1 < $len2 && $dat2[$i+1] ne ""){
+#		foreach(@dat){
+#			$tmp1 = "\"$dat2[$i]\"";
+#			$tmp1 =~ s/\(/\\\(/g;
+#			$tmp1 =~ s/\)/\\\)/g;
+#			$tmp1 =~ s/\[/\\\[/g;
+#			$tmp1 =~ s/\]/\\\]/g;
+#			$tmp2 = "\"$dat2[$i+1]\"";
+#			# if($_ =~ /$tmp1/){print $_;}
+#			$_ =~ s/$tmp1/$tmp2/g;
+#		}
+#	}
+#}
+
+foreach(@dat){
+	if(index($_,"\"") != -1){
+		for($i=0;$i<$len2;$i+=2){
+			if($i+1 < $len2 && $dat2[$i+1] ne ""){
+				$tmp1 = "\"$dat2[$i]\"";
+				$tmp1 =~ s/\(/\\\(/g;
+				$tmp1 =~ s/\)/\\\)/g;
+				$tmp1 =~ s/\[/\\\[/g;
+				$tmp1 =~ s/\]/\\\]/g;
+				$tmp1 =~ s/\+/\\\+/g;
+				$tmp1 =~ s/\_/\\\_/g;
+				$tmp1 =~ s/\-/\\\-/g;
+				$tmp1 =~ s/\|/\\\|/g;
+				$tmp2 = "\"$dat2[$i+1]\"";
+				$_ =~ s/$tmp1/$tmp2/g;
+			}
 		}
 	}
 }
-foreach(@dat){
-	print $_;
-}
-exit;
+
+#foreach(@dat){
+#	print $_;
+#}
+#exit;
 
 open(FH,">$filename");
 foreach(@dat){
-	print FH $_,"\n";
+	print FH $_;
 }
 close(FH);
 
