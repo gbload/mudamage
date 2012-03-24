632f9535f6cd2e2e1f0f06d61009d3745bcf446c
abitfire <abitfire@5450c723-22eb-8cc7-e3e6-6b817c0dad10>
2012-03-24 05:04:40 +0000

    FlexSDK4.6に変更

--- trunk/MUDamage7/build.sh
+++ trunk/MUDamage7/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-~/lib/flex_sdk_4.5.1.21328/bin/mxmlc -static-link-runtime-shared-libraries=true ./src/MUDamage7.mxml -output ./target/bin-debug/MUDamage7.swf
+~/lib/flex_sdk_4.6.0.23201B/bin/mxmlc -static-link-runtime-shared-libraries=true ./src/MUDamage7.mxml -output ./target/bin-debug/MUDamage7.swf
 
 #~/lib/flex_sdk_4.5.0.20967/bin/mxmlc -runtime-shared-library-path=/home/sinlion/lib/flex_sdk_4.5.0.20967/frameworks/libs/framework.swc,framework_4.5.0.20967.swz,,framework_4.5.0.20967.swf ./src/MUDamage7.mxml -output ./target/bin-debug/MUDamage7.swf
 
